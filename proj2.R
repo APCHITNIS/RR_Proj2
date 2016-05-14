9938fd78767ce0cabf943b4f47e6d99b61517fb1
Amit Chitnis <apchitnis@gmail.com>
2016-05-14 11:56:58 -0400

    finished part 1 and 2

--- proj2.R
+++ proj2.R
@@ -12,12 +12,12 @@ dfInjuries = sqldf("select EVTYPE, INJURIES from `dataMod` group by EVTYPE order
 #select top 10 rows for analysis
 dfInjuries=dfInjuries[1:10,]
 
-#group records by EVTYPE and order by PROPDMGVAL - Property Damage
-dfPropDmg = sqldf("select EVTYPE, PROPDMGVAL from `dataMod` group by EVTYPE order by PROPDMGVAL desc")
+#group records by EVTYPE and order by PROPDMG - Property Damage
+dfPropDmg = sqldf("select EVTYPE, PROPDMG from `dataMod` group by EVTYPE order by PROPDMG desc")
 #select top 10 rows for analysis
 dfPropDmg=dfPropDmg[1:10,]
-#group records by EVTYPE and order by CROPDMGVAL - Crop Damage
-dfCropDmg = sqldf("select EVTYPE, CROPDMGVAL from `dataMod` group by EVTYPE order by CROPDMGVAL desc")
+#group records by EVTYPE and order by CROPDMG - Crop Damage
+dfCropDmg = sqldf("select EVTYPE, CROPDMG from `dataMod` group by EVTYPE order by CROPDMG desc")
 #select top 10 rows for analysis
 dfCropDmg=dfCropDmg[1:10,]
 
@@ -29,22 +29,20 @@ barplot(dfFatalalies$FATALITIES, las = 3, names.arg = dfFatalalies$EVTYPE, main 
         ylab = "Number of fatalities", col = cols,  col.lab="black", cex.lab=0.75)
 
 #Display the event with the highest Injuries in red and others in blue
-cols <- c("blue", "red")[(dfPropDmg$INJURIES == max(dfInjuries$INJURIES)) + 1]
+cols <- c("blue", "red")[(dfInjuries$INJURIES == max(dfInjuries$INJURIES)) + 1]
 barplot(dfInjuries$INJURIES, las = 3, names.arg = dfInjuries$EVTYPE, main = "Events with Highest Injuries", 
         ylab = "Number of Injuries", col = cols,  col.lab="black", cex.lab=0.75)
 
 
 #Plot Property damage and Crop damage plots together
 par(mfrow = c(1, 2), mar = c(12, 4, 3, 2), mgp = c(3, 1, 0), cex = 0.8)
-cols <- c("blue", "red")[(dfPropDmg$PROPDMGVAL == max(dfPropDmg$PROPDMGVAL)) + 1]
+cols <- c("blue", "red")[(dfPropDmg$PROPDMG == max(dfPropDmg$PROPDMG)) + 1]
 
-barplot(dfPropDmg$PROPDMGVAL, las = 3, names.arg = dfPropDmg$EVTYPE, main = "Events with Highest Fatalities", 
+barplot(dfPropDmg$PROPDMG, las = 3, names.arg = dfPropDmg$EVTYPE, main = "Events with Highest Property Damage", 
         ylab = "Number of fatalities", col = cols,  col.lab="black", cex.lab=0.75)
 
-cols <- c("blue", "red")[(dfInjuries$INJURIES == max(dfInjuries$INJURIES)) + 1]
-barplot(dfInjuries$INJURIES, las = 3, names.arg = dfInjuries$EVTYPE, main = "Events with Highest Injuries", 
+cols <- c("blue", "red")[(dfCropDmg$CROPDMG == max(dfCropDmg$CROPDMG)) + 1]
+barplot(dfCropDmg$CROPDMG, las = 3, names.arg = dfCropDmg$EVTYPE, main = "Events with Highest Crom Damage", 
         ylab = "Number of Injuries", col = cols,  col.lab="black", cex.lab=0.75)
 
-
-plot(x = dfFatalalies$EVTYPE, y = dfFatalalies$FATALITIES, type="h", main="Top 10 Fatalities per disaster" , xlab="Disaster", ylab="Fatalities", col="blue", lwd=5, par(las=3))
 
